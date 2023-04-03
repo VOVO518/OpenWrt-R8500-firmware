1a6ff77441beaa958233e0ed88380ea0ecf1dc96
VOVO518 <129350523+VOVO518@users.noreply.github.com>
2023-04-03 16:43:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,11 +19,7 @@ sed -i "s/hostname='OpenWrt'/hostname='Nighthawk-X8'/g" package/base-files/files
 cat package/base-files/files/bin/config_generate |grep hostname=
 echo '=========Alert hostname OK!========='
 
-# 取消原主题luci-theme-bootstrap 为默认主题
+# 取消原主题luci-theme-bootstrap 为默认主题 - 修改 argon 为默认主题 - 删除原默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-
-# 修改 argon 为默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# 删除原默认主题
 rm -rf package/lean/luci-theme-bootstrap
